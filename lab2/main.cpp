26211d01837285b59af208bba9dc193e1ca334a5
Giovani Nascimento Pereira <giovani.x.pereira@gmail.com>
2019-11-22 17:44:38 -0300

    Finished reading all values

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -11,6 +11,10 @@ struct Node {
     int nodename, posx, posy, itemValue, itemWeight;
 };
 
+struct Edge {
+    int end1, end2, weight;
+};
+
 void checkOpeningParameters(int argc);
 
 int main(int argc, char *argv[]) {
@@ -35,6 +39,8 @@ int main(int argc, char *argv[]) {
     while (file >> type) { break; }
     file >> capacity >> P >> s >> t;
 
+    nedges = (nedges * 2);
+
     // DEBUG -----------
     cout << dispose << endl;
     cout << nnodes << "\t" << nedges << "\t" << type << "\t" << capacity << "\t" << P << "\t" << s << "\t" << t << endl;
@@ -43,10 +49,32 @@ int main(int argc, char *argv[]) {
     // Read Node values
     Node nodes[nnodes];
 
-    getline(file, dispose); // first line break
+    getline(file, dispose); // line break
     getline(file, dispose); // second line
     for (int i = 0; i < nnodes; i++) {
         file >> nodes[i].nodename >> nodes[i].posx >> nodes[i].posy >> nodes[i].itemValue >> nodes[i].itemWeight;
+    }
+
+    // Read Edges
+    Edge edges[nedges];
+
+    getline(file, dispose); // line break
+    getline(file, dispose); // second line
+    for (int i = 0; i < nedges; i++) {
+        file >> edges[i].end1 >> edges[i].end2 >> edges[i].weight;
+    }
+
+    // Create GUROBI Env
+    // GRBVar x[employeeAmount][employeeAmount][numberOfGroups];
+
+    try {
+        GRBEnv env = GRBEnv(true);
+        env.start();
+
+        GRBModel model = GRBModel(env);
+
+    } catch (exception e) {
+        cout << "Error during optimization: " << endl;
     }
 
     return 0;
